efa41a69bf4b1f6868c2b54a2feab4a214a4ba7b
LU <a830425tw@gmail.com>
2021-02-26 19:09:22 +0800

    加入yifeng的member/role的 getter/setter

--- src/main/java/_10_member/entity/Member.java
+++ src/main/java/_10_member/entity/Member.java
@@ -82,5 +82,134 @@ public class Member implements Serializable{
 	        )
 	private Set<Role> roles;
 
+	 
+	 
+	public Member() {
+		super();
+	}
 
+	public Integer getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(Integer memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMemberType() {
+		return memberType;
+	}
+
+	public void setMemberType(String memberType) {
+		this.memberType = memberType;
+	}
+
+	public Date getRegisterTime() {
+		return registerTime;
+	}
+
+	public void setRegisterTime(Date registerTime) {
+		this.registerTime = registerTime;
+	}
+
+	public Blob getImage() {
+		return image;
+	}
+
+	public void setImage(Blob image) {
+		this.image = image;
+	}
+
+	public Clob getMemo() {
+		return memo;
+	}
+
+	public void setMemo(Clob memo) {
+		this.memo = memo;
+	}
+
+	public ShopBean getShopBean() {
+		return shopBean;
+	}
+
+	public void setShopBean(ShopBean shopBean) {
+		this.shopBean = shopBean;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+
+	 
+	 
+	 
 }
--- src/main/java/_10_member/entity/Role.java
+++ src/main/java/_10_member/entity/Role.java
@@ -41,6 +41,54 @@ public class Role {
 	
 	@ManyToMany(mappedBy = "roles")
 	private Set<Member> member;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getRole_code() {
+		return role_code;
+	}
+
+
+	public void setRole_code(String role_code) {
+		this.role_code = role_code;
+	}
+
+
+	public String getRole_name() {
+		return role_name;
+	}
+
+
+	public void setRole_name(String role_name) {
+		this.role_name = role_name;
+	}
+
+
+	public Set<Member> getMember() {
+		return member;
+	}
+
+
+	public void setMember(Set<Member> member) {
+		this.member = member;
+	}
+
+
+	public Role() {
+		super();
+	}
+	
+	
+	
 	
 }
 
