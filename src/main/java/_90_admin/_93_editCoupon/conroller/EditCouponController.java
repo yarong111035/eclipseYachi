33412ffbdc8929250e520d6243956fdb706c0f00
javazh015 <s1050919@g.yzu.edu.tw>
2021-03-05 11:37:19 +0800

    新增_93_editCoupon 及 admin_coupon增加form

---
+++ src/main/java/_90_admin/_93_editCoupon/conroller/EditCouponController.java
@@ -0,0 +1,67 @@
+package _90_admin._93_editCoupon.conroller;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.http.HttpRequest;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialBlob;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import _02_model.entity.CouponBean;
+import _02_model.entity.ProductBean;
+
+
+@Controller
+public class EditCouponController {
+	
+//	表單頁面控制器方法
+	@GetMapping("/admin_coupon")
+	public String getAddNewProductFormString(Model model) {
+		CouponBean cb = new CouponBean();
+		cb.setCoupon_id(001);
+		cb.setCoupon_name("林筱珍");
+		cb.setCoupon_amount(30);
+		cb.setCoupon_days(5);
+		model.addAttribute("couponBean", cb); 
+		return "_16_admin/admin_coupon";
+	}
+	
+}
