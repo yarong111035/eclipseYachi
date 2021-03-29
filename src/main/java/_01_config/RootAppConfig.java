5e533680d553a0a4bb14f08120f61acf3002d67e
yuchijun <dazhi5379@gmail.com>
2021-03-29 11:09:08 +0800

    個人密碼修改

--- src/main/java/_01_config/RootAppConfig.java
+++ src/main/java/_01_config/RootAppConfig.java
@@ -24,7 +24,7 @@ public class RootAppConfig {
 	public DataSource dataSource() {
 		ComboPooledDataSource ds = new ComboPooledDataSource();
 		ds.setUser("root");
-		ds.setPassword("ABCDE123");
+		ds.setPassword("a19198889");
 		try {
 			ds.setDriverClass("com.mysql.cj.jdbc.Driver");
 		} catch (PropertyVetoException e) {
